4656b8e43f30fda02655825ac8fac80576b3e973
KotaroYuzawa <ktr.yzw@gmail.com>
2023-03-09 09:47:24 +0100

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="/home">Startseite</a></li>
+        <li><a href="/tanks/tankSearch">Tankdaten ansehen</a></li>
+        <li><a href="/tanks/tankAdd">Tankdaten eintragen</a></li>
+        <li><a href="/cars/carShow">Dienstwagen ansehen</a></li>
+        <li><a href="/cars/carAdd">Dienstwagen registrieren</a></li>
+        <li><a href="/drivers/driverShow">Fahrer ansehen</a></li>
+        <li><a href="/drivers/driverAdd">Fahrer registrieren</a></li>
+    </ul>
+</nav>
