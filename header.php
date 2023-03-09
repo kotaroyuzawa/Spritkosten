25647084c8774567e442501c555e340d7c7d0d83
KotaroYuzawa <ktr.yzw@gmail.com>
2023-03-09 09:48:59 +0100

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tankdaten eintragen</title>
-    <link rel="stylesheet" href="../public/style.css">
-</head>
-<body>
